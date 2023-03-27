2939917d0c059d9bca2f5c133100034f66f975d3
London Isla L <islois@melkki.cs.helsinki.fi>
2023-03-27 22:17:40 +0300

    H-tyo dict luotu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -2,8 +2,23 @@ import unittest
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
-    def setUp(self):
+   def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa")
+
+    def test_lataus_oikein(self):
+        self.assertNotEqual(self.Kassapaate)
+
+    def test_ottaminen_toimii(self):
+	if self.assertEqual(self.kassapaate):
+		return True
+	else:
+		return False
+
+
+
